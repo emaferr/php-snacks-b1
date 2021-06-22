df934748825f5d3e2751351ea3e17c8be26df990
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-22 15:24:14 +0200

    Create GET parameters Assign the parameters to input in the form

---
+++ Snack_2/index.php
@@ -0,0 +1,46 @@
+<?php
+//Passare come parametri GET name, mail e age e verificare
+var_dump($_GET['name']);
+var_dump($_GET['email']);
+var_dump($_GET['age']);
+
+//che name sia più lungo di 3 caratteri, 
+//che mail contenga un punto e una chiocciola 
+//che age sia un numero. 
+//Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+
+<body>
+
+    <h1>Log in</h1>
+
+    <form action="">
+        <label for="name">NAME</label>
+        <input type="text" name="name" id="name">
+        <label for="name">EMAIL</label>
+        <input type="email" name="email" id="email">
+        <label for="name">AGE</label>
+        <input type="number" name="age" id="age">
+   
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <div class="result">
+
+    </div>
+    
+</body>
+
+</html>
